d81adb0423d59a43712505d1711a8ce87c08d3c1
duuujin <enwls5626@gmail.com>
2026-02-10 15:12:58 +0000

    마이페이지 프로토타입 CREATE BUT 에러

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     RegisterView, 
@@ -16,6 +16,6 @@ urlpatterns = [
     path('change_password/', ChangePasswordView.as_view(), name='auth_change_password'),
     path('login/kakao/', KakaoLoginView.as_view(), name='kakao_login'),
     path('login/google/', GoogleLoginView.as_view(), name='google_login'),
+    # 슬래시 유무에 관계없이 매칭되도록 re_path 사용 고려 또는 기본 path 유지
     path('mypage/', MyPageView.as_view(), name='user_mypage'),
-    
 ]
