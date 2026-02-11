4e568d388dd03802f936eb4d6be03cd367a80f2d
mijin.develop <mijin.develop@gmail.com>
2026-02-12 02:00:09 +0900

    feat: sidebar component 추가 및 적용

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { ReactNode } from "react";
+import { LayoutWrapper } from "@/components/LayoutWrapper";
 import "@/styles/global.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="ko">
-			<body className="bg-black">{children}</body>
+			<body className="bg-black">
+				<LayoutWrapper>{children}</LayoutWrapper>
+			</body>
 		</html>
 	);
 }
