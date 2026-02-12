6416ddfc7b62ddc5a71b95a8579460e518a20bfb
mijin.develop <mijin.develop@gmail.com>
2026-02-12 10:39:08 +0900

    delete: auth comment 삭제

--- frontend/src/api/comment.ts
+++ frontend/src/api/comment.ts
@@ -7,24 +7,19 @@ export interface ShortsComment {
 	created_at: string;
 }
 
-// 댓글 목록 조회 응답
 export interface CommentListResponse {
 	movie_id: number;
 	comments: ShortsComment[];
 }
-
-// 댓글 작성 응답
 export interface CommentCreateResponse extends ShortsComment {
 	message: string;
 }
 
-// 댓글 삭제 응답
 export interface CommentDeleteResponse {
 	comment_id: number;
 	message: string;
 }
 
-// 1. 댓글 목록 조회
 export const fetchComments = async (
 	movie_id: string | number,
 ): Promise<CommentListResponse> => {
@@ -32,7 +27,6 @@ export const fetchComments = async (
 	return data;
 };
 
-// 2. 댓글 작성
 export const createComment = async (
 	movie_id: string | number,
 	content: string,
@@ -43,7 +37,6 @@ export const createComment = async (
 	return data;
 };
 
-// 3. 댓글 삭제
 export const deleteComment = async (
 	movie_id: string | number,
 	comment_id: string | number,
